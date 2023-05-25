be0358e4833e52bf348a206066828011cc85e599
donghae <todonghae@gmail.com>
2023-05-25 14:51:14 +0900

    배포 수정

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import ToDoItem from './TodoItem';
 import Sortable from 'sortablejs';
 
